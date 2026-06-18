e9a525d1ea1e997d447f1d99c32c98d185a8afe5
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-06-18 18:44:18 +0530

    Sequence Datatype

---
+++ Sequence.py
@@ -0,0 +1,11 @@
+Data1 = [10, 20, 30, 40, 50]
+Data2 = (10, 20, 30, 40, 50)
+Data3 = {10, 20, 30, 40, 50}
+Data4 = {"A" : 10, "B" : 20, "C" : 30, "D" : 40, "E" : 50}
+Data5 = "MArvellous Infosystems"
+
+print(type(Data1))
+print(type(Data2))
+print(type(Data3))
+print(type(Data4))
+print(type(Data5))
