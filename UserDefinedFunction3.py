91a2ba8227c756c78e165dbf024da3109d61b194
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-06-23 21:55:33 +0530

    Parameters accept onr parameter and return one value

---
+++ UserDefinedFunction3.py
@@ -0,0 +1,11 @@
+#Accept : One Parameter
+#Return : Nothing
+
+def Marvellous(Value):
+    print("Inside marvellous",Value)
+
+def main():
+    Marvellous(11)
+
+if __name__ == "__main__":
+    main()
