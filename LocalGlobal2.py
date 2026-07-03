764f27514fb847391a155b6c5625902d65c332f0
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-07-03 12:55:21 +0530

    Accesing local variable in other function

---
+++ LocalGlobal2.py
@@ -0,0 +1,14 @@
+no = 11
+
+def Display():
+    a = 21
+
+    print("From Display : ", no)
+    print("From Display value of a is : ", a)
+
+def Demo():
+    print("From Demo : ", no)
+    print("From Demo value of a is : ", a)       #Error
+
+Display()
+Demo()
