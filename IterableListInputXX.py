9e6beb59d02462aaadff7b1d7d165844ddc9b2f3
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-07-03 13:20:08 +0530

    initialization using list

---
+++ IterableListInputXX.py
@@ -0,0 +1,24 @@
+def main():
+    Marks = list()
+
+    print("Enter the 5 elements : ")
+
+    no = int(input())
+    Marks.append(no)
+
+    no = int(input())
+    Marks.append(no)
+
+    no = int(input())
+    Marks.append(no)
+
+    no = int(input())
+    Marks.append(no)
+
+    no = int(input())
+    Marks.append(no)
+
+    print(Marks)
+
+if __name__ == "__main__":
+    main()
