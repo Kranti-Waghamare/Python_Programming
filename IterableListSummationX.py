0a21ff60220aa3ce80193b7f7b0fbee4b79b80d6
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-07-03 10:58:50 +0530

    Addition using iterable

---
+++ IterableListSummationX.py
@@ -0,0 +1,12 @@
+def main():
+    Marks = [78, 95, 60, 55, 37]
+
+    Sum = 0
+
+    for no in Marks:
+        Sum = Sum + no
+
+    print("Addition is : ",Sum)
+
+if __name__ == "__main__":
+    main()
