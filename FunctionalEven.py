21371a6950345de757927efec76ee1b76b7b49fa
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-07-04 12:45:55 +0530

    Check even odd using lambda function

---
+++ FunctionalEven.py
@@ -0,0 +1,14 @@
+ChkEven = lambda No : (No % 2 == 0)
+         
+def main():
+    Value = int(input("Enter the number : "))
+
+    Ret = ChkEven(Value)
+
+    if(Ret == True):
+        print("It is even")
+    else:
+        print("It is odd")
+
+if __name__ == "__main__":
+    main()
