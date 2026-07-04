ffbd73609bb97f30f2811b38f451525566c79425
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-07-04 13:36:21 +0530

    import user defined library

---
+++ MarvellousFMRClient.py
@@ -0,0 +1,27 @@
+from MarvellousLibrary import filterX, mapX, reduceX
+
+ChkEven = lambda No : (No % 2 == 0)
+
+Increment = lambda No : No + 1
+
+Addition = lambda No1, No2 : No1 + No2
+
+def main():
+    Data = [13, 12, 8, 11, 15, 10]
+
+    print("Input Data is : ",Data)
+
+    FData = list(filterX(ChkEven, Data))
+
+    print("Data after filter : ",FData)
+
+    MData = list(mapX(Increment, FData))
+
+    print("Data after map : ", MData)
+
+    RData = reduceX(Addition, MData)
+
+    print("Data after reduce : ", RData)
+
+if __name__ == "__main__":
+    main()
