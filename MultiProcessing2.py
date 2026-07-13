8a5f3b51f3656a68509b526806cef44107b42ba9
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-07-13 19:57:56 +0530

    multiprocessing with its pid and ppid

---
+++ MultiProcessing2.py
@@ -0,0 +1,48 @@
+# 2 + 4 + 6 + 8 = 20
+
+import os
+import time
+import multiprocessing
+
+def SumEven(No):
+    print(f"PID of SumEven : {os.getpid()} PPID of SumEven : {os.getppid()}")
+
+    Sum = 0
+
+    for i in range(2, No, 2):
+        Sum = Sum + i
+
+    print("Summation of Even is : ",Sum)
+
+#1 + 3 + 5 + 7 + 9 = 25
+
+def SumOdd(No):
+    print(f"PID of SumOdd : {os.getpid()} PPID of SumOdd : {os.getppid()}")
+
+    Sum = 0
+
+    for i in range(1, No, 2):
+        Sum = Sum + i
+
+    print("Summation of Odd is : ",Sum)
+
+def main():
+    print(f"PID of main : {os.getpid()} PPID of main : {os.getppid()}")
+
+    start_time = time.perf_counter()
+
+    t1 = multiprocessing.Process(target= SumEven, args= (100000000,))
+    t2 = multiprocessing.Process(target= SumOdd, args= (100000000,))
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
+    
+    end_time = time.perf_counter()
+
+    print(f"Time required is : {end_time - start_time:.4f}seconds")
+
+if __name__ == "__main__":
+    main()    
