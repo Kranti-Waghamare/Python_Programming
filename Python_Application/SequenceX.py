189d60dac5b1060d52df507f9c33df3884762eb4
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-08-05 19:40:06 +0530

    Sequence datatype of python

---
+++ Python_Application/SequenceX.py
@@ -0,0 +1,12 @@
+Data1 = (10, 20, 30, 40, 50)              #Tuple
+Data2 = [10, 20, 30, 40, 50]              #List
+Data3 = {10, 20, 30, 40, 50}               #Set
+Data4 = {"A" : 10, "B" : 20, "C" : 30, "D" : 40, "E" : 50}      #Dict
+Data5 = "Maharashtra"                       #str
+
+
+print(type(Data1))
+print(type(Data2))
+print(type(Data3))
+print(type(Data4))
+print(type(Data5))
