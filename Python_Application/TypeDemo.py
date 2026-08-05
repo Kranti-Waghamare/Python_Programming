61e6b5caf830cda7dd9eabfd797c0e2aeece563b
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-08-05 19:35:24 +0530

    Type of data

---
+++ Python_Application/TypeDemo.py
@@ -0,0 +1,19 @@
+no = 11             #int
+marks = 89.90       #float
+value = 8 + 9j      #complex
+
+print(value)
+
+print(type(no))
+print(type(marks))
+print(type(value))
+
+Gender = True
+
+print(Gender)
+print(type(Gender))
+
+Demo = None
+
+print(Demo)
+print(type(Demo))
