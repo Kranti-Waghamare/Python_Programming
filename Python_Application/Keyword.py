f5e43bcdb338278dffc3e9747a3ff8bca74b35d7
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-08-06 13:49:38 +0530

    Keyword parameter

--- Python_Application/Keyword.py
+++ Python_Application/Keyword.py
@@ -1,13 +1,10 @@
-def AreaCircle(Radius, PI = 3.14):
+def AreaCircle(Radius, PI):
     Area = PI * Radius * Radius
     return Area
 
 def main():
-    Ret = AreaCircle(10.4)
+    Ret = AreaCircle(Radius = 10.4, PI = 3.14)
     print("Area of circle is : ", Ret)
-
-    Ret = AreaCircle(4.3, 7.12)
-    print("Area of Circle is : ", Ret)
 
 if __name__ == "__main__" :
     main()
