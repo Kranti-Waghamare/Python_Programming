f36c8e1a9266527d1e9c3adee655840e2d0fbc1c
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-08-06 12:53:42 +0530

    Addition of two numbers inside main function

---
+++ Python_Application/Function1.py
@@ -0,0 +1,13 @@
+def main():
+    print("Enter the first number : ")
+    Value1 = int(input())
+
+    print("Enter the second number : ")
+    Value2 = int(input())
+
+    Result = Value1 + Value2
+
+    print("Addition is : ",Result)
+
+if __name__ == "__main__":
+    main()
