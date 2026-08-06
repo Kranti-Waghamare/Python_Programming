c7389867294db68227c5a1d03a46685a890f5c3c
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-08-06 13:47:36 +0530

    Default parameter

---
+++ Python_Application/Default.py
@@ -0,0 +1,13 @@
+def AreaCircle(Radius, PI = 3.14):
+    Area = PI * Radius * Radius
+    return Area
+
+def main():
+    Ret = AreaCircle(10.4)
+    print("Area of circle is : ", Ret)
+
+    Ret = AreaCircle(4.3, 7.12)
+    print("Area of Circle is : ", Ret)
+
+if __name__ == "__main__" :
+    main()
