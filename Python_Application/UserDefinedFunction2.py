97f3500b0f140c5dadce65ffed114738939a91eb
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-08-06 13:14:06 +0530

    accept one parameter return nothing

---
+++ Python_Application/UserDefinedFunction2.py
@@ -0,0 +1,12 @@
+#Accept : One parameter
+#Return : Nothing
+
+
+def Marvellous(Value):
+    print("Inside main : ", Value)
+
+def main():
+    Marvellous(11)
+
+if __name__ == "__main__":
+    main()
