c673eaedd555b3eed94a39c45e995ef67ea130d2
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-08-06 13:39:25 +0530

    Inner function wil only accessible within the body of outer(Enclosing) function where it is defined

---
+++ Python_Application/UserDefinedFunction9.py
@@ -0,0 +1,14 @@
+def BigBazar():
+    print("Inside the Big Bazar")
+
+    def Amul():
+        print("Inside the Amul Icecreame parlor")
+
+    Amul()
+    Amul()
+
+def main():
+    BigBazar()        #Allowed
+
+if __name__ == "__main__":
+    main()
