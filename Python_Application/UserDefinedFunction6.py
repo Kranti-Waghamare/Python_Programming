13f74ffe554bd80b50dfe27514e18c91676ad8fd
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-08-06 13:24:47 +0530

    two functions accepts two parameters and return single value

---
+++ Python_Application/UserDefinedFunction6.py
@@ -0,0 +1,16 @@
+def Multiplication(No1, No2):
+    return No1 * No2
+
+def Division(No1, No2):
+    return No1 / No2
+   
+
+def main():
+    Ret1 = Multiplication(11, 10)
+    print("Multiplication is : ", Ret1)
+
+    Ret1 = Division(15, 3)
+    print("Division is : ", Ret1)
+
+if __name__ == "__main__":
+    main()
