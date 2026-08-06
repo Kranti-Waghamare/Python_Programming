f519088150820de42a5c68018256d581ccbeff2e
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-08-07 02:29:57 +0530

    Summation of list

---
+++ Python_Application/IterableListSummationX.py
@@ -0,0 +1,12 @@
+def main():
+    Marks = [45, 56, 78, 89, 96, 43]
+
+    Sum = 0
+
+    for no in Marks:
+        Sum = Sum + no
+
+    print("Addition is : ", Sum)
+    
+if __name__ == "__main__":
+    main()
