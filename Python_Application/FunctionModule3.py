598c5548df4c05eaead9aada01111adaefdbdc98
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-08-06 13:05:12 +0530

    anotheer type of module import

---
+++ Python_Application/FunctionModule3.py
@@ -0,0 +1,15 @@
+from Marvellous import Addition 
+
+def main():
+    print("Enter the first number : ")
+    Value1 = int(input())
+
+    print("Enter the second number : ")
+    Value2 = int(input())
+
+    Result = Addition(Value1, Value2)           
+  
+    print("Addition is : ",Result)
+
+if __name__ == "__main__":
+    main()
