3f0d1505f79c520d70825901fc333773784a95d2
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-08-10 20:23:48 +0530

    Check even by using the filter and lambda

---
+++ Python_Application/FilterXX.py
@@ -0,0 +1,13 @@
+CheckEven = lambda No : No % 2 == 0
+
+def main():
+    Data = [12, 13, 8, 19, 22, 45, 66, 86]
+
+    print("Input data is  : ",Data)
+
+    FData = list(filter(CheckEven, Data))
+
+    print("Data after filter : " , FData)
+
+if __name__ == "__main__":
+    main()
