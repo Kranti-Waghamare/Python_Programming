0d1de75c3f0c6006a4955e449b50fec3505fee47
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-08-10 20:26:56 +0530

    map function

---
+++ Python_Application/MapX.py
@@ -0,0 +1,22 @@
+def CheckEven(No):
+    return(No % 2 == 0)
+
+def Increment(No):
+    return (No + 1)
+
+
+def main():
+    Data = [12, 13, 8, 19, 22, 45, 66, 86]
+
+    print("Input data is  : ",Data)
+
+    FData = list(filter(CheckEven, Data))
+
+    print("Data after filter : " , FData)
+
+    MData = list(map(Increment, FData))
+
+    print("Data after map : ", MData)
+
+if __name__ == "__main__":
+    main()
