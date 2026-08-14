f975123a1acde6c7195efb6006749345db2deba3
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-08-14 11:16:08 +0530

    we can write multiple except block to handle the exception

---
+++ Python_Application/ExceptionDemo3.py
@@ -0,0 +1,26 @@
+def main():
+    Ans = 0
+
+    try:
+        print("Enter the first number : ")
+        No1 = int(input())
+
+        print("Enter the second number : ")
+        No2 = int(input())
+
+        Ans =  No1 / No2
+
+        print("Division is successful")
+
+    except ZeroDivisionError as zobj:
+
+        print("Exception occur due to second operand is zero : ",zobj)
+
+    except ValueError as vobj:
+
+        print("Exception occur due to Invalid datatype : ",vobj)
+
+    print("Result is : ", Ans)
+
+if __name__ == "__main__":
+    main()
