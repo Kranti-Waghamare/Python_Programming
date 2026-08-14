8c137d8054f8a9afcc25828142279df3f977d231
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-08-14 11:12:48 +0530

    Divison of two numbers by handling the exception

---
+++ Python_Application/ExceptionDemo2.py
@@ -0,0 +1,22 @@
+def main():
+    Ans = 0
+
+    try:
+        print("Enter the first number : ")
+        No1 = int(input())
+
+        print("Enter the second number : ")
+        No2 = int(input())
+
+        Ans =  No1 / No2
+
+        print("Division is successful")
+
+    except ZeroDivisionError as zobj:
+
+        print("Exception occur due to second operand is zero : ",zobj)
+
+        print("Result is : ", Ans)
+
+if __name__ == "__main__":
+    main()
