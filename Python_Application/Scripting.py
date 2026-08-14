b5371cd9b906eb4707e10bf7b498ed151732ad7f
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-08-15 01:26:35 +0530

    program with scripting programming

---
+++ Python_Application/Scripting.py
@@ -0,0 +1,13 @@
+print("Enter the first number : ")
+No1 = int(input())
+
+print("Enter the second number : ")
+No2 = int(input())
+
+Ans = No1 + No2
+
+print("Addition is : ",Ans)
+
+Ans = No1 - No2
+
+print("Substraction is : ",Ans)
