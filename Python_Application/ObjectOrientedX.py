2418a4cacb0917ac6a629503e249248849985c37
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-08-15 01:40:13 +0530

    Issue solved by adding the self argument

---
+++ Python_Application/ObjectOrientedX.py
@@ -0,0 +1,26 @@
+class Arithmetic:
+    def Addition(self, No1, No2):
+        Ans = No1 + No2
+
+        return Ans
+
+    def Substraction(self, No1, No2):
+        Ans = No1 - No2
+
+        return Ans
+
+Aobj = Arithmetic()
+
+print("Enter the first number : ")
+Value1 = int(input())
+
+print("Enter the second number : ")
+Value2 = int(input())
+
+Ret = Aobj.Addition(Value1, Value2)     #ISSUE/ERROR
+
+print("Addition is : ", Ret)
+
+Ret = Aobj.Substraction(Value1, Value2)   #ISSUE/ERROR
+
+print("Substraction is : ", Ret)
