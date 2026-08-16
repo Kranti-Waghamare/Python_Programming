6782f57060f6b77be003df73e8860ca18ed86be3
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-08-16 23:15:30 +0530

    It calls the both the methods inside the base and derived class

---
+++ Python_Application/Inheritence5.py
@@ -0,0 +1,12 @@
+class Base:
+    def fun(self):
+        print("Inside base fun")
+ 
+class Derived(Base):
+    def sun(Self):
+        print("Inside derived sun")
+
+bobj = Derived()
+
+bobj.fun()
+bobj.sun()
