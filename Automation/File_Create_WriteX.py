ee32c7ddb89aae663954cdd0e586dea251d0b7e8
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-08-19 21:20:01 +0530

    The data gets override due to repeated write call on the same file

---
+++ Automation/File_Create_WriteX.py
@@ -0,0 +1,14 @@
+def main():
+    try:
+        fobj = open("Demo.txt","w")
+        print("File gets Opend")
+
+        fobj.write("Marvellous Infosystems")
+
+        fobj.close()
+
+    except FileNotFoundError as fobj:
+        print("File is not present in the current directory")
+
+if __name__ == "__main__":
+    main()
