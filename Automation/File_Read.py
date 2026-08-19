78ec42930fc9c62ac2d12535e09c67713f5a2917
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-08-19 21:25:42 +0530

    Data gets read due to read method

---
+++ Automation/File_Read.py
@@ -0,0 +1,16 @@
+def main():
+    try:
+        fobj = open("Demo.txt","r")
+        print("File gets Opend")
+
+        Data = fobj.read(10)
+
+        print(Data)
+
+        fobj.close()
+
+    except FileNotFoundError as fobj:
+        print("File is not present in the current directory")
+
+if __name__ == "__main__":
+    main()
