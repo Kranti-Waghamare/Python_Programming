87b4f8fe83202d56bcdf06aa3b1d72c35511cc3d
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-08-19 21:33:33 +0530

    How much data we gave to read that changes the offset of the file

---
+++ Automation/File_Read_tellX.py
@@ -0,0 +1,26 @@
+def main():
+    try:
+        fobj = open("Demo.txt","r")
+        print("File gets Opend")
+
+        print("File offset is : ", fobj.tell())
+
+        Data = fobj.read(10)
+        
+        print(Data)
+
+        print("File offset is : ", fobj.tell())
+
+        Data = fobj.read(10)
+                
+        print(Data)
+
+        print("File offset is : ", fobj.tell())
+        
+        fobj.close()
+
+    except FileNotFoundError as fobj:
+        print("File is not present in the current directory")
+
+if __name__ == "__main__":
+    main()
