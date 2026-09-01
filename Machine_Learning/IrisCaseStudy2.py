a8bdedd5d01a6a2b663c8d487361ee62a1fbc04f
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-09-01 23:56:59 +0530

    Dataset with features and target

---
+++ Machine_Learning/IrisCaseStudy2.py
@@ -0,0 +1,20 @@
+from sklearn.datasets import load_iris
+
+def main():
+    print("-"*30)
+    print("Laod Iris dataset")
+    print("-"*30)
+
+    Dataset = load_iris()
+
+    # Metadataset of the dataset
+    print("Independent variable are : ")
+    print(Dataset.feature_names)
+
+    print("Dependent variable ate : ") 
+    print(Dataset.target_names)
+
+    print(Dataset)
+
+if __name__ == "__main__":
+    main()
